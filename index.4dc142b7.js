424507444d5d2995b944c70188cdc78c800f0c83
Alina Kabanets <kabanets.dolls@gmail.com>
2024-01-16 14:40:50 +0000

    make build

---
+++ index.4dc142b7.js
@@ -0,0 +1,3 @@
+var e=document.querySelector(".gallery"),t=document.querySelector(".gallery__large-img");e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".list-item__link").getAttribute("href");t.src=r});//# sourceMappingURL=index.4dc142b7.js.map
+
+//# sourceMappingURL=index.4dc142b7.js.map
